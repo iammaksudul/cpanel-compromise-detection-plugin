8898285c1f26bea7be6bb38bd273b8a63c156515
iammaksudul <56459189+iammaksudul@users.noreply.github.com>
2025-02-09 07:29:05 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ mkdir -p /usr/local/cpanel/whostmgr/docroot/cgi/cpanel-compromise-detection/
 # Copy the correct script files from the current directory
 cp /root/cpanel-compromise-detection-plugin/cpanel-compromise-detection.sh /usr/local/cpanel/whostmgr/docroot/cgi/cpanel-compromise-detection/
 cp /root/cpanel-compromise-detection-plugin/index.html /usr/local/cpanel/whostmgr/docroot/cgi/cpanel-compromise-detection/
+cp /root/cpanel-compromise-detection-plugin/plugin.conf /usr/local/cpanel/whostmgr/docroot/cgi/cpanel-compromise-detection/
+cp /root/cpanel-compromise-detection-plugin/whm_plugin.conf /usr/local/cpanel/whostmgr/docroot/cgi/cpanel-compromise-detection/
 
 # Create a cPanel configuration file for the plugin
 cat > /usr/local/cpanel/whostmgr/docroot/cgi/cpanel-compromise-detection/cpanel-compromise-detection.conf << EOF
@@ -27,5 +29,12 @@ EOF
 
 # Set the appropriate permissions
 chmod -R 755 /usr/local/cpanel/whostmgr/docroot/cgi/cpanel-compromise-detection/
+
+# Ensure the plugin.conf and whm_plugin.conf files have correct permissions as well
+chmod 644 /usr/local/cpanel/whostmgr/docroot/cgi/cpanel-compromise-detection/plugin.conf
+chmod 644 /usr/local/cpanel/whostmgr/docroot/cgi/cpanel-compromise-detection/whm_plugin.conf
+
+# Restart cPanel services to make the plugin active
+/scripts/restartsrv_cpsrvd
 
 echo "Installation completed. Access your plugin from WHM!"
